5f125600777ccae200378acfbdc17f599b2d39a8
Crow314 <master@crow31415.net>
2020-10-12 01:14:32 +0900

    PID書き込み機能実装

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module crow31415.net/clicker-back
 
 go 1.15
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	github.com/facebookgo/atomicfile v0.0.0-20151019160806-2de1f203e7d5 // indirect
+	github.com/facebookgo/pidfile v0.0.0-20150612191647-f242e2999868
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/facebookgo/pidfile"
+)
+
 func main() {
+	savePid()
+	defer removePid()
+
 	println("Hello, world")
+}
+
+func savePid() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	path := fmt.Sprintf("%s/%s", dir, "tmp/pids/app.pid")
+	pidfile.SetPidfilePath(path)
+	err = pidfile.Write()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func removePid() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	path := fmt.Sprintf("%s/%s", dir, "tmp/pids/app.pid")
+	err = os.Remove(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
